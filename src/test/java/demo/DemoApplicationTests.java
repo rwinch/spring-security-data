ec65515165fccce5958e5060683d3ddf2dee45cf
Rob Winch <rwinch@gopivotal.com>
2015-06-17 10:57:37 -0500

    Run as rob

--- src/test/java/demo/DemoApplicationTests.java
+++ src/test/java/demo/DemoApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -17,6 +18,7 @@ public class DemoApplicationTests {
 	@Autowired
 	MyDomainRepository repository;
 
+	@WithMockUser("rob")
 	@Test
 	public void findAllOnlyFindsAllowed() {
 		Page<MyDomain> results = repository.findAll(new PageRequest(0, 10));
