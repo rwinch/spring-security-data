7c980dac938351977132383e08037dad4ab3854a
Rob Winch <rwinch@gopivotal.com>
2015-06-17 10:55:03 -0500

    Attempt at permission sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,7 @@ insert into my_domain values (2, 'attribute 2 rob');
 
 insert into my_domain values (10, 'attribute 1 luke');
 insert into my_domain values (11, 'attribute 2 luke');
+
+
+insert into permission (id,domain_yype,domain_id, permission, username) values(1, 'demo.MyDomain', 1, 'read', 'rob');
+insert into permission (id,domain_yype,domain_id, permission, username) values(2, 'demo.MyDomain', 2, 'read', 'rob');
